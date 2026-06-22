b28c711f09bc6e95985a5b300014cb9c6e858572
NOGiTTiS <Knight_Darkwing@hotmail.com>
2026-06-22 08:06:12 +0700

    deploy vercel fix prisma

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,11 @@
 import { PrismaClient } from '@prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
+import { Pool } from 'pg'
 
-const adapter = new PrismaPg(process.env.DATABASE_URL as string)
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+})
+const adapter = new PrismaPg(pool)
 const prisma = new PrismaClient({ adapter })
 
 async function main() {
